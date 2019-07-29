de46ed2cd187e96909821207e5cd354649a412a4
Saheed <sdrfngg640@gmail.com>
2019-07-29 18:13:09 +0100

    Add reset, display, submit and remove functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, read){
+    constructor(title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -11,20 +11,53 @@ class Book {
 
 
 function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  display()
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    reset();
+    display();
 }
 
-function display(){ 
-    myLibrary.forEach((book) => {
-        const title = document.createElement('span')
-        book.title;
-
-        book.author;
-        
-        book.pages;
-
-    })
-
+function reset() {
+    let tbody = document.getElementById('table-body');
+    tbody.innerHTML = '';
 }
+
+
+function display() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let body = `<tr>
+                        <th scope="row">${i+1}</th>
+                        <td>${myLibrary[i].title}</td>
+                        <td>${myLibrary[i].author}</td>
+                        <td>${myLibrary[i].pages}</td>
+                        <td class="btn btn-md"><button>${myLibrary[i].read}</button></td>
+                        <button id="remove" data-index-number="${i}">Remove</button>
+                    </tr>`;
+        let tbody = document.getElementById('table-body');
+        tbody.innerHTML += body;
+    }
+}
+
+document.getElementById('add').addEventListener("submit", (e) => {
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked;
+    addBookToLibrary(title, author, pages, read);
+});
+
+
+let remove = document.getElementById("remove");
+let dataset = remove.dataset.indexNumber;
+console.log("dataset");
+document.getElementById("remove").addEventListener("click", function() {
+    let index = parseInt(remove.dataset.indexNumber);
+    myLibrary.splice(index, 1);
+    reset();
+    display();
+});
+
+// addBookToLibrary("First title", "First author", 23, true);
+// addBookToLibrary("First title", "First author", 23, true);
+// addBookToLibrary("First title", "First author", 23, true);
